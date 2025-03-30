0d8011799bfdb251df2d497d10206f92adf19e1f
Your NameJabanes <erezhabani2003@gmail.com>
2025-03-30 18:19:49 +0300

    cors allow origin false listed origins added

--- backend/myproj/settings.py
+++ backend/myproj/settings.py
@@ -153,4 +153,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://instabot-ca8d9.web.app",
+    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+]
